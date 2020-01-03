910376008fee1a7c7a76b9fd941ed28eff63bb30
jac <crowja@gmail.com>
2020-01-02 19:52:45 -0600

    Update Doxygen headers.

--- pvec.c
+++ pvec.c
@@ -2,9 +2,8 @@
  *  @file pvec.c
  *  @version 0.1.1-dev0
  *  @date Sat Dec  7 13:08:23 CST 2019
- *  @copyright %COPYRIGHT%
- *  @brief FIXME
- *  @details FIXME
+ *  @copyright 2020 John A. Crow <crowja@gmail.com>
+ *  @license Unlicense <http://unlicense.org/>
  */
 
 #include <stdlib.h>
